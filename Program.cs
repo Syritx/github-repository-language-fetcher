c508ac4d77dd3ad9d8bfb2a6278e498cb549f2f7
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-08-14 12:44:35 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace GithubRespositoryNameFetcher
+{
+    class MainClass
+    {
+        static string a;
+        static IWebDriver driver;
+
+        public static void Main(string[] args)
+        {
+            driver = new FirefoxDriver();
+            string url = "https://github.com/Syritx?tab=repositories";
+            fetchNames(url);
+        }
+
+
+        static void fetchNames(string url)
+        {
+            driver.Url = url;
+            Thread.Sleep(10000);
+
+            int repositories = 30;
+            for (int i = 0; i < repositories; i++)
+            {
+                int id = i + 1;
+                try {
+
+                    IWebElement repository;
+                    IWebElement language;
+
+                    repository = driver.FindElement(By.XPath("/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div/div[2]/ul/li["+id+"]/div[1]/div[1]/h3/a"));
+                    language = driver.FindElement(By.XPath("/html/body/div[4]/main/div[2]/div/div[2]/div[2]/div/div[2]/ul/li["+id+"]/div[1]/div[3]/span/span[2]"));
+
+                    Console.WriteLine("[NAME]: {0}, [LANGUAGE]: {1} \n",repository.Text, language.Text);
+                }
+                catch(Exception e) {}
+            }
+        }
+    }
+}
